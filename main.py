60007ba06711914c604a18d5769b576527fb7c28
Marcio Vinicius de Souza da Rocha <60201245+MarciovsRocha@users.noreply.github.com>
2022-08-10 23:16:44 +0000

    add comentários no código

---
+++ main.py
@@ -0,0 +1,6 @@
+# Marcio Vinicius de Souza da Rocha
+# documentação no arquivo README
+# este VENV do replit está hospedado em 
+# um [repositório do github](https://github.com/MarciovsRocha/Maquina_Estados_Finito)
+
+# code...
